418117c48d256a7f1a5ce42fc920232747535f0b
aleng1106 <ltlobygg@163.com>
2024-12-13 16:12:37 +0800

    cart item

--- src/store/modules/takeaway.js
+++ src/store/modules/takeaway.js
@@ -6,6 +6,7 @@ const foodStore = createSlice({
   initialState: {
     foodList: [],
     activeIndex: 0,
+    cartList: [],
   },
   reducers: {
     setFoodList(state, action) {
@@ -14,10 +15,18 @@ const foodStore = createSlice({
     changeActiveIndex(state, action) {
       state.activeIndex = action.payload;
     },
+    addCart(state, action) {
+      const item = state.cartList.find((item) => item.id === action.payload.id);
+      if (item) {
+        item.count++;
+      } else {
+        state.cartList.push(action.payload);
+      }
+    },
   },
 });
 
-const { setFoodList, changeActiveIndex } = foodStore.actions;
+const { setFoodList, changeActiveIndex, addCart } = foodStore.actions;
 const fetchFoodList = () => {
   return async (dispatch) => {
     const res = await axios.get("http://localhost:3004/takeaway");
@@ -25,6 +34,6 @@ const fetchFoodList = () => {
   };
 };
 
-export { setFoodList, fetchFoodList, changeActiveIndex };
+export { setFoodList, fetchFoodList, changeActiveIndex, addCart };
 
 export default foodStore.reducer;
